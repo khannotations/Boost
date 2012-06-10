dd6d6b33c72b3a3f6891dac7b92008d9fb2e695a
Rafi Khan <faiaz.a.khan@gmail.edu>
2012-06-10 19:57:01 -0400

    acclaim tab

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,4 +8,8 @@ class MainController < ApplicationController
 
   def author
   end
+
+  def acclaim
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Boost::Application.routes.draw do
   root :to => 'main#index'
   match "book" => "main#book"
   match "author" => "main#author"
+  match "acclaim" => "main#acclaim"
 
 
 end
