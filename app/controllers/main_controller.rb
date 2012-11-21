524b095bc62ace6c5a733270ac583adb0cc7a363
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-21 14:54:43 -0600

    fixed routes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,14 +2,5 @@ class MainController < ApplicationController
 
   def index
   end
-
-  def book
-  end
-
-  def author
-  end
-
-  def acclaim
-  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Boost::Application.routes.draw do
 
   root :to => 'main#index'
-  match "book" => "main#book"
-  match "author" => "main#author"
-  match "acclaim" => "main#acclaim"
-
 
 end
