bfb6dc6cd40a83f560a29184da59ace360b6beab
Rafi Khan <faiaz.a.khan@gmail.com>
2013-01-07 20:57:19 -0600

    changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
 
   def index
   end
+
+  def about
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Boost::Application.routes.draw do
 
   root :to => 'main#index'
+  match 'about' => 'main#about'
 
 end
